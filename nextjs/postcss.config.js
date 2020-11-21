d67eabf7382179fdb475003f9bf256c0bf0ad7c4
Alex Patterson <alex@ajonp.com>
2020-11-21 12:47:41 -0500

    adding back in postcss

--- nextjs/postcss.config.js
+++ nextjs/postcss.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: ["tailwindcss", "autoprefixer"],
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            // the paths to all template files
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : false,
+    "postcss-preset-env",
+  ],
 };
--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ["./**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
