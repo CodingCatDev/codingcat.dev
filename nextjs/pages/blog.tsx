ae7ef80f8965f268f93696e2533894ae14fe25ba
Alex Patterson <alex@ajonp.com>
2020-11-23 10:31:44 -0500

    update blog

--- nextjs/pages/blog.tsx
+++ nextjs/pages/blog.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
+import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+
 import config from "../configureAmplify";
 import gql from "graphql-tag";
-
 import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
 
-const listPosts = gql`
-  query listPosts {
-    listPosts {
+const postsByStatusPublish = gql`
+  query postsByStatusPublish {
+    postsByStatusPublish(sortDirection: DESC, post_status: "publish") {
       items {
-        post_title
         id
-        post_content
+        post_title
+        post_thumbnail
+        post_publish_datetime
+        post_excerpt
+        post_permalink
       }
     }
   }
@@ -28,27 +33,55 @@ const client = new AWSAppSyncClient({
     keyPrefix: "public",
   },
 });
-export default function Posts() {
+
+export default function Blog() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     fetchPosts();
     async function fetchPosts() {
       const postData: any = await client.query({
-        query: listPosts,
+        query: postsByStatusPublish,
       });
-      // console.log(postData.data.listPosts.items);
-      setPosts(postData.data.listPosts.items);
+      setPosts(postData.data.postsByStatusPublish.items);
     }
   }, []);
-
   return (
     <div>
-      <h1>Posts</h1>
-      {posts.map((post) => (
-        <Link href={`/blog/${post.id}`} key={post.id}>
-          <a>{post.post_title}</a>
-        </Link>
-      ))}
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-2 place-items-auto">
+        {posts.map((post) => (
+          <div className="bg-white shadow p-3 m-3 rounded" key={post.id}>
+            <div>
+              <Link href={post.post_permalink}>
+                <>
+                  <Image
+                    src={post.post_thumbnail}
+                    alt={post.post_title}
+                    width="480"
+                    height="270"
+                    layout="responsive"
+                    className="rounded cursor-pointer"
+                  />
+                </>
+              </Link>
+            </div>
+            <div className="mt-6">
+              <p className="text-lg text-bold tracking-wide text-gray-600 mb-2">
+                {post.post_title}
+              </p>
+              <p className="text-sm text-gray-600 font-hairline">
+                {post.post_excerpt}
+              </p>
+            </div>
+          </div>
+        ))}
+      </main>
+
+      <footer></footer>
     </div>
   );
 }
