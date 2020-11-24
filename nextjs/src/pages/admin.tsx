30f4e69ddab1c4548734ed8e68078d0310ac67b6
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-24 16:54:41 -0500

    amplify pull

--- nextjs/src/pages/admin.tsx
+++ nextjs/src/pages/admin.tsx
@@ -1,15 +1,35 @@
 import Head from "next/head";
-import PostsCards from "../components/PostsCards";
+import { useEffect, useState } from "react";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import config from "../../configureAmplify";
 
-export default function Blog() {
+export default function Admin() {
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<any | undefined>();
+  console.log(`Login using`, config.aws_appsync_region);
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
   return (
     <div>
       <Head>
         <title>Admin | CodingCatDev</title>
+        <meta name="robots" content="noindex" />
       </Head>
 
-      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-2 place-items-auto">
-        <PostsCards post_type={"post"} />
+      <main className="">
+        {authState === AuthState.SignedIn && user ? (
+          <div className="App">
+            <div>Hello, {user.username}</div>
+            <AmplifySignOut />
+          </div>
+        ) : (
+          <AmplifyAuthenticator />
+        )}
       </main>
 
       <footer></footer>
