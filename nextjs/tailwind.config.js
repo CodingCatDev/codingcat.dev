ef9daee7399c283a8e72f018b6d8f34e957d2a97
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-24 11:44:31 -0500

    add back article style

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -47,88 +47,48 @@ module.exports = {
         },
       },
     },
-    transform: {
-      // defaults to this value
-      none: "none",
-    },
-    transformOrigin: {
-      // defaults to these values
-      t: "top",
-      tr: "top right",
-      r: "right",
-      br: "bottom right",
-      b: "bottom",
-      bl: "bottom left",
-      l: "left",
-      tl: "top left",
-    },
-    translate: {
-      // defaults to {}
-      "1/2": "50%",
-      full: "100%",
-      "right-up": ["100%", "-100%"],
-      "3d": ["40px", "-60px", "-130px"],
-    },
-    scale: {
-      // defaults to {}
-      90: "0.9",
-      100: "1",
-      110: "1.1",
-      "-100": "-1",
-      "stretched-x": ["2", "0.5"],
-      "stretched-y": ["0.5", "2"],
-      "3d": ["0.5", "1", "2"],
-    },
-    rotate: {
-      // defaults to {}
-      90: "90deg",
-      180: "180deg",
-      270: "270deg",
-      "3d": ["0", "1", "0.5", "45deg"],
-    },
-    skew: {
-      // defaults to {}
-      "-5": "-5deg",
-      5: "5deg",
-    },
-    perspective: {
-      // defaults to {}
-      none: "none",
-      250: "250px",
-      500: "500px",
-      750: "750px",
-      1000: "1000px",
-    },
-    perspectiveOrigin: {
-      // defaults to these values
-      t: "top",
-      tr: "top right",
-      r: "right",
-      br: "bottom right",
-      b: "bottom",
-      bl: "bottom left",
-      l: "left",
-      tl: "top left",
-    },
-  },
-  variants: {
-    // all the following default to ['responsive']
-    transform: ["responsive"],
-    transformOrigin: ["responsive"],
-    translate: ["responsive"],
-    scale: ["responsive"],
-    rotate: ["responsive"],
-    skew: ["responsive"],
-    perspective: ["responsive"],
-    perspectiveOrigin: ["responsive"],
-    transformStyle: ["responsive"],
-    backfaceVisibility: ["responsive"],
-    transformBox: ["responsive"],
+    // typography: (theme) => ({
+    //   default: {
+    //     css: {
+    //       "h1,\nh2,\nh3,\nh4,\nh5,\nh6": {
+    //         margin: "1.5rem 0 1rem 0",
+    //         fontFamily: "nunito",
+    //         fontWeight: "normal",
+    //         lineHeight: 1.2,
+    //         color: theme('colors.ccd-primary.700'),
+    //         "&:first-child": { marginTop: "0" }
+    //       },
+    //       h1: { fontSize: "2.5rem" },
+    //       h2: { fontSize: "2rem" },
+    //       h3: { fontSize: "1.75rem" },
+    //       h4: { fontSize: "1.5rem" },
+    //       h5: { fontSize: "1.25rem" },
+    //       h6: { fontSize: "1rem" },
+    //       mark: {
+    //         background: "#fff8e1",
+    //         padding: "0.25rem 0.4rem",
+    //         borderRadius: "$borderRadius",
+    //         fontFamily: "monospace"
+    //       },
+    //       blockquote: {
+    //         margin: "1rem 0",
+    //         padding: "0 2rem",
+    //         borderLeft: "4px solid #90a4ae"
+    //       },
+    //       hr: {
+    //         borderTop: "solid $dividerColor",
+    //         borderWidth: "1px 0 0 0",
+    //         margin: "1rem 0"
+    //       },
+    //       p: {
+    //         margin: "0 0 1rem 0",
+    //         lineHeight: 1.5,
+    //       }
+    //     },
+    //   },
+    // }),
   },
   plugins: [
-    require("tailwindcss-transforms")({
-      "3d": false, // defaults to false
-    }),
-    require("tailwindcss-animatecss"),
+    require('@tailwindcss/typography'),
   ],
 };
