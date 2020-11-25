99f61966ade862e84580fb179a3798d10544d1d1
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-25 13:43:38 -0500

    index page working

--- backend-cdk/lambdas/Post.ts
+++
@@ -1,7 +0,0 @@
-type Post = {
-  id: string;
-  title: string;
-  content: string;
-};
-
-export default Post;
---
+++ nextjs/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig = {
+  apiKey: "AIzaSyCVRqmtn64AV8oS54TgIQXvJPpAqwo7FCI",
+  authDomain: "dev-codingcat-dev.firebaseapp.com",
+  databaseURL: "https://dev-codingcat-dev.firebaseio.com",
+  projectId: "dev-codingcat-dev",
+  storageBucket: "dev-codingcat-dev.appspot.com",
+  messagingSenderId: "231660301280",
+  appId: "1:231660301280:web:f0b0de33df240439119985",
+  measurementId: "G-7H8BMTVY4G",
+};
