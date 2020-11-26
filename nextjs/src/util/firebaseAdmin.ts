8b95778e92673f2c2e314bc3f3274c5e27f00fa4
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-26 07:36:49 -0500

    THIS IS GROSS REMOVE

--- nextjs/src/util/firebaseAdmin.ts
+++ nextjs/src/util/firebaseAdmin.ts
@@ -3,6 +3,7 @@ import * as admin from "firebase-admin";
 import { serviceAccountKey, config } from "../config/firebase";
 
 if (admin.apps.length === 0) {
+  console.log(serviceAccountKey)
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccountKey),
       databaseURL: config.databaseURL,
