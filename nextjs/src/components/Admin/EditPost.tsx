705ae05ea1ffd41b8a79ca497220c72447b1fe97
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-30 14:21:32 -0500

    getting post data to text edit

--- nextjs/src/components/Admin/EditPost.tsx
+++ nextjs/src/components/Admin/EditPost.tsx
@@ -1,21 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
 import dynamic from 'next/dynamic';
 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/saga-purple/theme.css';
 
-import { useUser } from '@/utils/auth/useUser';
 import { getPost } from '@/services/firestore';
 
-const MdxEditor = dynamic(() => import('@/components/MdxEditor'), {
-  ssr: false,
-  loading: () => <p>Chasing a mouse...</p>,
-});
-
-function EditPost({ className }) {
+function EditPost({ className, router }) {
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    getPost(
+      router.query.type === 'blog'
+        ? `/posts/${router.query.id}`
+        : `/${router.query.type}/${router.query.id}`
+    ).then((postRef) => {
+      setPost(postRef);
+    });
+  }, [router]);
   return (
     <div className={className}>
-      <MdxEditor />
+      <textarea
+        id="about"
+        name="about"
+        rows={10}
+        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md resize-none"
+        placeholder="Markdown goes here..."
+        value={post ? post.post_content : ''}
+      ></textarea>
     </div>
   );
 }
--- nextjs/src/pages/admin/[type]/[id].tsx
+++ nextjs/src/pages/admin/[type]/[id].tsx
@@ -3,7 +3,6 @@ import dynamic from 'next/dynamic';
 import { withRouter } from 'next/router';
 
 import AdminMenu from '@/components/Admin/AdminMenu';
-import MdxEditor from '@/components/MdxEditor';
 
 const EditPost = dynamic(() => import('@/components/Admin/EditPost'), {
   ssr: false,
@@ -11,30 +10,24 @@ const EditPost = dynamic(() => import('@/components/Admin/EditPost'), {
 });
 
 function AdminDashboard({ router }) {
-  const path = `/${router.asPath.substring(
-    router.asPath.lastIndexOf('/') + 1
-  )}`;
   return (
     <div>
       <Head>
-        <title>
-          {`${path.substr(1).substr(0, 1).toUpperCase()}${path.substr(2)}`} |
-          CodingCatDev
-        </title>
+        <title>{`Edit ${router.query.type}| CodingCatDev`}</title>
         <meta name="robots" content="noindex" />
       </Head>
 
       <main>
         <>
-          <div className="h-screen flex overflow-hidden bg-gray-100">
+          <div className="flex overflow-hidden bg-gray-100">
             <AdminMenu router={router} />
             <div className="flex flex-col w-0 flex-1 overflow-hidden pt-16">
               <main
                 className="flex-1 relative overflow-y-auto focus:outline-none"
                 tabIndex={0}
               >
-                <div className="m-6 border-2 border-ccd-primary-800 bg-white flex flex-col">
-                  <EditPost className="flex-grow" />
+                <div className="m-6">
+                  <EditPost className="h-48 bg-blue-200" router={router} />
                 </div>
               </main>
             </div>
