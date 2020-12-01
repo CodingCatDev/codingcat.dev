3cb86b4e6d8b9af5d7934921abc12943a7b7dfcf
Alex Patterson <ajonp@alexs-mac-mini.lan>
2020-11-30 22:15:45 -0500

    basic tab preview

--- nextjs/src/components/Admin/EditPost.tsx
+++ nextjs/src/components/Admin/EditPost.tsx
@@ -10,6 +10,7 @@ import { postDataObservable, postUpdate } from '@/services/api';
 function EditPost({ router }) {
   const [post, setPost] = useState(null);
   const [path, setPath] = useState(null);
+  const [tab, setTab] = useState('edit');
   // Sets initial state
   useEffect(() => {
     const path =
@@ -19,7 +20,6 @@ function EditPost({ router }) {
     setPath(path);
     postDataObservable(path).subscribe((post) => {
       setPost(post);
-      console.log(post);
     });
   }, [router]);
 
@@ -27,15 +27,52 @@ function EditPost({ router }) {
     postUpdate(path, event.target.value);
     setPost(event.target.value);
   }
+  function selectTab(tab) {
+    setTab(tab);
+  }
   return (
-    <textarea
-      id="about"
-      name="about"
-      onChange={handleChange}
-      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block h-full w-full sm:text-sm border-gray-300 rounded-md resize-none"
-      placeholder="Markdown goes here..."
-      value={post ? post.post_content : ''}
-    ></textarea>
+    <>
+      <div>
+        <ul className="flex cursor-pointer">
+          <li
+            className={`py-2 px-6 bg-white rounded-t-lg ${
+              tab === 'edit' ? '' : 'text-gray-500 bg-gray-200'
+            }`}
+            onClick={() => selectTab('edit')}
+          >
+            Edit
+          </li>
+          <li
+            className={`py-2 px-6 bg-white rounded-t-lg ${
+              tab === 'preview' ? '' : 'text-gray-500 bg-gray-200'
+            }`}
+            onClick={() => selectTab('preview')}
+          >
+            Preview
+          </li>
+        </ul>
+      </div>
+      <textarea
+        id="about"
+        name="about"
+        onChange={handleChange}
+        className={`${
+          tab === 'edit' ? 'block' : 'hidden'
+        } shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block h-full w-full sm:text-sm border-gray-300 rounded-md resize-none`}
+        placeholder="Markdown goes here..."
+        value={post ? post.post_content : ''}
+      ></textarea>
+      <textarea
+        id="about"
+        name="about"
+        onChange={handleChange}
+        className={`${
+          tab === 'preview' ? 'block' : 'hidden'
+        } shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block h-full w-full sm:text-sm border-gray-300 rounded-md resize-none`}
+        placeholder="Markdown goes here..."
+        value={`preview`}
+      ></textarea>
+    </>
   );
 }
 
