03857cc1a13f880c5b0f0228e65b1e4a38cf1ded
brittneypostma <brittney.postma@hotmail.com>
2021-02-12 15:14:07 -0500

    still working on table

--- frontend/main/src/components/admin/EditPosts.tsx
+++ frontend/main/src/components/admin/EditPosts.tsx
@@ -1,80 +1,140 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { useUser } from '@/utils/auth/useUser';
 import { postsByUpdatedAtObservable } from '@/services/api';
 import { Post, PostStatus, PostType } from '@/models/post.model';
+import { take } from 'rxjs/operators';
 
 function EditPosts({ type }: { type: PostType }) {
   const { user, signout }: { user: any; signout: any } = useUser();
+
   const [posts, setPosts] = useState<Post[]>([]);
+
   useEffect(() => {
-    postsByUpdatedAtObservable(type).subscribe((posts) => setPosts(posts));
+    postsByUpdatedAtObservable(type)
+      .pipe(take(1))
+      .subscribe((posts) => setPosts(posts));
   }, [type]);
+
   return (
-    <div>
-      {posts.map((post, index) => (
-        <table
-          className="flex w-full mb-4 lg:mb-0 lg:grid text-basics-900"
-          key={post.id}
+    <table className="block w-full lg:table bg-basics-50" role="table">
+      <tbody role="rowgroup">
+        <tr
+          className="hidden lg:flex bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50"
+          role="row"
         >
-          <thead className="grid gap-4 p-2 flex-full lg:gap-0 bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50">
-            <tr className="grid justify-between w-full h-full grid-cols-1 text-left lg:flex">
-              <th>Title</th>
-              <th>Id</th>
-              <th>Date</th>
-              <th>Author</th>
-              <th>Status</th>
+          <th className="p-2 text-left" role="columnheader">
+            Title
+          </th>
+          <th className="p-2 text-left" role="columnheader">
+            Id
+          </th>
+          <th className="p-2 text-left" role="columnheader">
+            Date
+          </th>
+          <th className="p-2 text-left" role="columnheader">
+            Author
+          </th>
+          <th className="p-2 text-left" role="columnheader">
+            Status
+          </th>
+        </tr>
+
+        {posts.map((post, index) => (
+          <tr className="flex" key={post.id}>
+            <tr
+              className="grid flex-1 w-full lg:flex bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50"
+              role="row"
+            >
+              <th className="p-2 text-left lg:hidden" role="columnheader">
+                Title
+              </th>
+              <th className="p-2 text-left lg:hidden" role="columnheader">
+                Id
+              </th>
+              <th className="p-2 text-left lg:hidden" role="columnheader">
+                Date
+              </th>
+              <th className="p-2 text-left lg:hidden" role="columnheader">
+                Author
+              </th>
+              <th className="p-2 text-left lg:hidden" role="columnheader">
+                Status
+              </th>{' '}
             </tr>
-          </thead>
-
-          <Link href={`${type}/${post.id}`}>
-            <a className="grid grid-cols-1 text-basics-900 lg:flex">
-              <tr
-                className={`w-full grid lg:flex justify-between +
-              ${index % 2 ? 'bg-primary-100' : 'bg-basics-50'}`}
-              >
-                <td className="p-2 border-b lg:border-r border-primary-900 lg:border-b-0">
-                  {post.title}
-                </td>
-                <td className="p-2 border-b lg:border-r border-primary-900 lg:border-b-0">
-                  {post.id}
-                </td>
-                <td className="p-2 border-b lg:border-r border-primary-900 lg:border-b-0">
-                  {post.updatedAt}
-                </td>
-                <td className="p-2 border-b lg:border-r border-primary-900 lg:border-b-0">
-                  {post.createdBy}
-                </td>
-                <td className="p-2">
-                  <span
-                    className={`m-1 ${
-                      post && post.status === PostStatus.draft
-                        ? `p-1 rounded-sm bg-primary-300 text-white`
-                        : `p-1 rounded-sm bg-secondary-400 text-secondary-900`
-                    }`}
-                  >
-                    {post.status}
-                  </span>
-                </td>
-              </tr>
-            </a>
-          </Link>
-        </table>
-      ))}
-
-      <style jsx>{`
-        @media (min-width: 1024px) {
-          div > table:not(:first-child) thead {
-            display: none;
-          } 
-        
-      `}</style>
-    </div>
+            <tr
+              className={`grid w-full lg:flex ${
+                index % 2 ? 'bg-primary-100' : 'bg-basics-50'
+              }`}
+            >
+              <td className="p-2" role="cell">
+                <Link href={`${type}/${post.id}`}>
+                  <a>{post.title} </a>
+                </Link>
+              </td>
+              <td className="p-2" role="cell">
+                {post.id}
+              </td>
+              <td className="p-2" role="cell">
+                {post.updatedAt}
+              </td>
+              <td className="p-2" role="cell">
+                {post.createdBy}
+              </td>
+              <td className="p-2" role="cell">
+                <span
+                  className={`m-1 ${
+                    post && post.status === PostStatus.draft
+                      ? `px-2 py-1 rounded-full bg-basics-400 text-white dark:bg-basics-400 dark:text-white`
+                      : `px-2 py-1 rounded-full bg-success-600 text-basics-50 dark:bg-success-600 dark:text-basics-50`
+                  }`}
+                >
+                  {post.status}
+                </span>
+              </td>
+            </tr>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
 export default EditPosts;
+
+// <section className="data-responsive-demo">
+//   <div className="card">
+//     <DataTable value={posts} className="p-datatable-responsive-demo">
+//       <Column
+//         className=""
+//         field="title"
+//         header="Title"
+//         body={titleBodyTemplate}
+//       />
+//       <Column className="" field="id" header="ID" body={idBodyTemplate} />
+//       <Column
+//         className=""
+//         field="date"
+//         header="Date"
+//         body={dateBodyTemplate}
+//       />
+//       <Column
+//         className=""
+//         field="author"
+//         header="Author"
+//         body={authorBodyTemplate}
+//       />
+//       <Column
+//         className=""
+//         field="status"
+//         header="Status"
+//         body={statusBodyTemplate}
+//       />
+//     </DataTable>
+//   </div>
+
 /* <table className="table-auto">
         <thead className="text-white bg-secondary-500 bold">
           <tr>
@@ -177,3 +237,47 @@ export default EditPosts;
   );
       
       */
+// const titleBodyTemplate = (rowData) => {
+//   return (
+//     <React.Fragment>
+//       <span className="p-column-title lg:hidden">Title</span>
+//       {rowData.title}
+//     </React.Fragment>
+//   );
+// };
+
+// const idBodyTemplate = (rowData) => {
+//   return (
+//     <React.Fragment>
+//       <span className="p-column-title lg:hidden">ID</span>
+//       {rowData.id}
+//     </React.Fragment>
+//   );
+// };
+
+// const dateBodyTemplate = (rowData) => {
+//   return (
+//     <React.Fragment>
+//       <span className="p-column-title lg:hidden">Date</span>
+//       {rowData.updatedAt}
+//     </React.Fragment>
+//   );
+// };
+
+// const authorBodyTemplate = (rowData) => {
+//   return (
+//     <React.Fragment>
+//       <span className="p-column-title lg:hidden">Author</span>
+//       {rowData.createdBy}
+//     </React.Fragment>
+//   );
+// };
+
+// const statusBodyTemplate = (rowData) => {
+//   return (
+//     <React.Fragment>
+//       <span className="p-column-title lg:hidden">Status</span>
+//       {rowData.status}
+//     </React.Fragment>
+//   );
+// };
