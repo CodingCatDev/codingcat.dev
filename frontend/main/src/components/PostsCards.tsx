e755a1d70645d31cef4db3f2b4af8f0f17139a93
brittneypostma <brittney.postma@hotmail.com>
2021-03-09 12:39:07 -0500

    added logic for paid/start/free and created badges for them

--- frontend/main/src/components/PostsCards.tsx
+++ frontend/main/src/components/PostsCards.tsx
@@ -4,6 +4,7 @@ import { Post } from '@/models/post.model';
 import AJPrimary from '@/components/global/icons/AJPrimary';
 
 export default function PostsCards({ posts }: { posts: Post[] }): JSX.Element {
+  console.log(posts);
   return (
     <>
       {posts && posts.length > 0 ? (
@@ -13,7 +14,7 @@ export default function PostsCards({ posts }: { posts: Post[] }): JSX.Element {
               <div className="rounded-md shadow bg-basics-50" key={post.id}>
                 <Link href={`/${post.type}/${post.slug}`}>
                   <a>
-                    {post.coverPhoto?.path ? (
+                    {/* {post.type === 'course' ? (
                       <>
                         <Image
                           src={post.coverPhoto?.path}
@@ -32,6 +33,63 @@ export default function PostsCards({ posts }: { posts: Post[] }): JSX.Element {
                         <div className="absolute flex items-center flex-auto w-full h-full rounded-t-md bg-primary-900 dark:bg-primary-900">
                           <AJPrimary className="w-full h-full p-4" />
                         </div>
+                      </div>
+                    )}  */}
+                    {post.coverPhoto?.path && post.type === 'course' ? (
+                      <div className="relative">
+                        <Image
+                          src={post.coverPhoto?.path}
+                          alt={post.title}
+                          width="480"
+                          height="270"
+                          layout="responsive"
+                          className="rounded-md rounded-b-none cursor-pointer"
+                        />
+                        {post.accessSettings?.accessMode === 'closed' ? (
+                          <div className="absolute grid gap-2 font-bold tracking-wider top-4 -right-8 place-items-center">
+                            <p className="px-4 py-2 rounded-full bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50">
+                              Paid
+                            </p>
+                            <p className="px-4 py-2 font-bold tracking-wider transform -skew-y-12 bg-primary-900 dark:bg-primary-900 text-basics-50 dark:text-basics-50">
+                              ${post.accessSettings?.price}
+                            </p>
+                          </div>
+                        ) : post.accessSettings?.accessMode === 'free' ? (
+                          <p className="absolute px-4 py-2 font-bold tracking-wider rounded-full bg-secondary-600 dark:bg-secondary-600 top-4 -right-8 text-basics-50 dark:text-basics-50">
+                            Free
+                          </p>
+                        ) : (
+                          <p className="absolute px-4 py-2 font-bold tracking-wider rounded-full bg-secondary-600 dark:bg-secondary-600 top-4 -right-8 text-basics-50 dark:text-basics-50">
+                            Start now
+                          </p>
+                        )}
+                      </div>
+                    ) : post.coverPhoto?.path ? (
+                      <>
+                        <Image
+                          src={post.coverPhoto?.path}
+                          alt={post.title}
+                          width="480"
+                          height="270"
+                          layout="responsive"
+                          className="rounded-md rounded-b-none cursor-pointer"
+                        />
+                      </>
+                    ) : (
+                      <div
+                        className="relative"
+                        style={{ paddingBottom: '56.25%' }}
+                      >
+                        <div className="absolute flex items-center flex-auto w-full h-full rounded-t-md bg-primary-900 dark:bg-primary-900">
+                          <AJPrimary className="w-full h-full p-4" />
+                        </div>
+                        {post.type === 'course' ? (
+                          <p className="absolute px-4 py-2 rounded-full bg-secondary-600 dark:bg-secondary-600 top-4 -right-8 text-basics-50 dark:text-basics-50">
+                            Start now
+                          </p>
+                        ) : (
+                          <></>
+                        )}
                       </div>
                     )}
                   </a>
