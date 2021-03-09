87fdec8245a74de4deff8557dda0b8a092fcad04
brittneypostma <brittney.postma@hotmail.com>
2021-03-09 09:11:50 -0500

    minor style/responsive updates

--- frontend/main/src/pages/404.tsx
+++ frontend/main/src/pages/404.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Error from 'next/error';
+import Link from 'next/link';
+import AJ404 from '@/components/global/icons/AJ404';
 
 import { getSite } from '@/services/serversideApi';
 import { Post } from '@/models/post.model';
@@ -7,7 +9,7 @@ import Layout from '@/layout/Layout';
 
 import { Site } from '@/models/site.model';
 
-export default function Home({
+export default function Custom404({
   site,
 }: {
   site: Site | null;
@@ -20,8 +22,18 @@ export default function Home({
       <Head>
         <title>404 | CodingCatDev</title>
       </Head>
-      <section className="grid justify-items-center place-items-center">
-        <h1>404 | This page could not be found</h1>
+      <section className="grid content-start grid-cols-1 gap-10 p-4 text-center justify-items-center">
+        <AJ404 />
+        <h1 className="text-5xl lg:text-6xl">
+          Uh oh, that page doesn&apos;t seem to exist.
+        </h1>
+        <h2 className="font-sans text-4xl lg:text-5xl">
+          Were you looking for{' '}
+          {/* add some logic here to say which route they clicked? */}
+          <Link href="/courses">
+            <a className="underline text-secondary-600">Courses</a>
+          </Link>
+        </h2>
       </section>
     </Layout>
   );
