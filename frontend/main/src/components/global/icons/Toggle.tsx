fca74dd18a1f6ce7e0ccc33671bc8a68ed78c970
Alex Patterson <alex@ajonp.com>
2021-03-17 23:03:32 -0400

    yellow is too much

--- frontend/main/src/components/global/icons/Toggle.tsx
+++ frontend/main/src/components/global/icons/Toggle.tsx
@@ -19,9 +19,9 @@ export default function Toggle() {
         <svg
           className="w-8"
           xmlns="http://www.w3.org/2000/svg"
-          fill="#FFC700"
+          fill="white"
           viewBox="0 0 24 24"
-          stroke="currentColor"
+          stroke="white"
         >
           <path
             strokeLinecap="round"
@@ -33,10 +33,10 @@ export default function Toggle() {
       ) : (
         <svg
           className="w-8"
-          fill="#FFC700"
+          fill="white"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          stroke="none"
+          stroke="white"
         >
           <path
             strokeLinecap="round"
