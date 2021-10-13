8f76b4c1ef80d83b4ba6013eb90977c8d486fd3e
Alex Patterson <alex@ajonp.com>
2021-10-13 08:33:35 -0400

    move hooks within provider tree

--- frontend/main/src/pages/user/membership.tsx
+++ frontend/main/src/pages/user/membership.tsx
@@ -1,67 +1,21 @@
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
-
 import Layout from '@/layout/Layout';
-import SettingsLinks from '@/components/settings/SettingsLinks';
-import FirebaseSignin from '@/components/FirebaseSignin';
 
-import { useState } from 'react';
 import { Site } from '@/models/site.model';
 import { getSite } from '@/services/serversideApi';
-import { useFunctions, useSigninCheck } from 'reactfire';
-import { httpsCallable } from 'firebase/functions';
+import UserMembershipDetail from '@/components/user/UserMembershipDetail';
 
-export default function Profile({ site }: { site: Site | null }): JSX.Element {
-  const { data: signInCheckResult } = useSigninCheck();
-  const functions = useFunctions();
-
-  const [loading, setLoading] = useState(false);
-  async function onStripePortal() {
-    setLoading(true);
-    const { url } = await (
-      await httpsCallable<unknown, { url: string }>(
-        functions,
-        'ext-firestore-stripe-subscriptions-createPortalLink'
-      ).call('params', { returnUrl: window.location.href })
-    )?.data;
-    window.location.assign(url);
-  }
-
+export default function Membership({
+  site,
+}: {
+  site: Site | null;
+}): JSX.Element {
   return (
     <Layout site={site}>
       <Head>
         <title>Profile | CodingCatDev</title>
       </Head>
-      {signInCheckResult?.signedIn === true ? (
-        <section className="grid self-start justify-center gap-10 p-10 lg:grid-cols-settings">
-          <section>
-            <h2 className="mb-4 font-sans text-4xl vertical-text-clip">
-              Settings
-            </h2>
-            <SettingsLinks />
-          </section>
-          <div className="grid">
-            <div>
-              <p className="text-4xl">CodingCat.dev uses the Stripe Portal.</p>
-              <p className="text-2xl">
-                You will find all payment and subscription statuses by clicking
-                the below button.
-              </p>
-              <button
-                className="mt-4 btn-primary"
-                onClick={() => onStripePortal()}
-              >
-                {loading ? 'Redirecting...' : 'Access Stripe Portal'}
-              </button>
-              <p className="mt-1">
-                *Please note this will redirect you away from codingcat.dev.
-              </p>
-            </div>
-          </div>
-        </section>
-      ) : (
-        <FirebaseSignin />
-      )}
+      <UserMembershipDetail />
     </Layout>
   );
 }
