637552022e2cda9eeddbe4bb7caa076f0b621904
Alex Patterson <alex@ajonp.com>
2021-10-30 12:07:12 -0400

    add post type

--- backend/sanity/schemas/schema.js
+++ backend/sanity/schemas/schema.js
@@ -1,13 +1,13 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import tag from './tag'
-import post from './post'
-import author from './author'
+import tag from './tag';
+import author from './author';
+import post from './post';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -18,8 +18,8 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
     author,
     tag,
+    post,
   ]),
-})
+});
