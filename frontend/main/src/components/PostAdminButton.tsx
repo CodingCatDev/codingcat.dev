a5eef0ed53223223fa4b7c79a1840b50d2133ab6
Alex Patterson <alex@ajonp.com>
2021-11-01 13:05:37 -0400

    hack the admin button

--- frontend/main/src/components/PostAdminButton.tsx
+++ frontend/main/src/components/PostAdminButton.tsx
@@ -1,7 +1,7 @@
 import useIsMember from '@/hooks/useIsMember';
 import { Post } from '@/models/post.model';
 import { UserInfoExtended } from '@/models/user.model';
-
+import { config } from '@/config/sanity';
 export default function CourseBuyButton({
   post,
   user,
@@ -12,10 +12,22 @@ export default function CourseBuyButton({
   const { member, team } = useIsMember(user);
 
   function adminLink() {
+    const getUrl = () => {
+      let u;
+      if (process.env.NODE_ENV === 'development') {
+        u = 'http://localhost:3333';
+      } else {
+        u =
+          config.dataset === 'main'
+            ? 'https://admin.codingcat.dev'
+            : `https://admin-${config.dataset}.codingcat.dev`;
+      }
+      return u;
+    };
     return (
       <>
         <a
-          href={`/admin/${post._type}/${post.postId}`}
+          href={`${getUrl()}/desk/${post._type};${post._id}`}
           target="_blank"
           rel="noreferrer"
           role="link"
