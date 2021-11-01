1b1f01fd38afebaeac44d56812312ad3f0ffa1cd
Alex Patterson <alex@ajonp.com>
2021-11-01 19:19:14 -0400

    allow live update stream

--- frontend/main/src/config/sanity.ts
+++ frontend/main/src/config/sanity.ts
@@ -2,6 +2,6 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   apiVersion: '2021-03-25',
-  useCdn: process.env.NODE_ENV === 'production',
+  useCdn: false,
   token: process.env.SANITY_API_TOKEN || '',
 };
