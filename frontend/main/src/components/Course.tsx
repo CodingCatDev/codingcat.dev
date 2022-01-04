31b7cb8c8b5d8b09c49b5aa182253745d3ecc60d
Alex Patterson <alex@codingcat.dev>
2022-01-03 21:24:09 -0500

    include course page locks

--- frontend/main/src/components/Course.tsx
+++ frontend/main/src/components/Course.tsx
@@ -16,6 +16,7 @@ import { MDXRemoteSerializeResult, MDXRemote } from 'next-mdx-remote';
 import { useSigninCheck } from 'reactfire';
 import CourseBuyButton from '@/components/CourseBuyButton';
 import { components } from '@/components/code/MDXComponents';
+import { AccessMode } from '@/models/access.model';
 
 export default function Course({
   post,
@@ -193,22 +194,22 @@ export default function Course({
                 {post.sections.map((section) => (
                   <section
                     key={section._key}
-                    className="flex flex-col bg-basics-50 rounded-t-md"
+                    className="flex flex-col rounded-t-md"
                   >
-                    <h2 className="p-2 m-0 text-2xl font-bold xl:p-4 rounded-t-md xl:flex-shrink-0 bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50">
+                    <div className="p-2 m-0 text-2xl font-bold xl:p-4 rounded-t-md xl:flex-shrink-0 bg-secondary-600 dark:bg-secondary-600 text-basics-50 dark:text-basics-50">
                       {section.title}
-                    </h2>
-                    <ul className="flex flex-col flex-grow justify-items-stretch">
+                    </div>
+                    <ul className="flex flex-col flex-grow rounded-b rounded-tr bg-basics-50 justify-items-stretch">
                       {section.lessons &&
                         section.lessons.map((lesson) => (
                           <li key={lesson._id} className="ml-0 list-none">
                             <Link
-                              href={`/course/${post.slug}/lesson/${lesson.slug}`}
+                              href={`/post/${post.slug}/lesson/${lesson.slug}`}
                               key={lesson._id}
                               passHref
                             >
                               <div
-                                className={`p-2  cursor-pointer hover:bg-primary-200
+                                className={`p-2 cursor-pointer hover:bg-primary-200 rounded m-1 flex flex-wrap justify-between
                               ${
                                 isActiveLink(post, lesson)
                                   ? 'bg-primary-200'
@@ -219,6 +220,25 @@ export default function Course({
                                 <a className="no-underline text-basics-900 hover:text-primary-900">
                                   {lesson.title}
                                 </a>
+                                {lesson?.accessSettings?.accessMode && (
+                                  <div className="no-underline text-basics-900 hover:text-primary-900">
+                                    {lesson?.accessSettings?.accessMode !=
+                                      AccessMode.free && (
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-5 h-5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                          clipRule="evenodd"
+                                        />
+                                      </svg>
+                                    )}
+                                  </div>
+                                )}
                               </div>
                             </Link>
                           </li>
