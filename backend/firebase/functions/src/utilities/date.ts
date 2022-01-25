41d7fe75361e6a67b2e28daf55f072df7aef9290
Alex Patterson <alex@codingcat.dev>
2022-01-25 17:23:07 -0500

    getOffset

--- backend/firebase/functions/src/utilities/date.ts
+++ backend/firebase/functions/src/utilities/date.ts
@@ -1,32 +1,22 @@
-export function utcOffset(isoString: string, timezone = 'America/New_York') {
+export function utcOffset(isoString: string, timeZone = 'America/new_york') {
   console.log('conversion:input', isoString);
-  const originalDate = new Date(
-    new Date(isoString).toLocaleString('en-US', {
-      timeZone: timezone,
-    })
-  );
-  const utcDate = new Date(
-    new Date(isoString).toLocaleString('en-US', {
-      timeZone: 'UTC',
-    })
-  );
-
-  // // Milliseconds from original Date
-  const offset = originalDate.getTime() - utcDate.getTime();
-  const output = new Date(Date.parse(originalDate.toISOString()) + offset)
+  const isoStringDate = new Date(isoString);
+  const offset = getOffset(timeZone, isoStringDate);
+  console.log('conversion:offsethours', offset / 60 / 60 / 1000);
+  const output = new Date(Date.parse(isoStringDate.toISOString()) + offset)
     .toISOString()
     .replace('Z', '');
   console.log('conversion:output', output);
   return output;
 }
 
+const getOffset = (timeZone = 'UTC', date = new Date()) => {
+  const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
+  const tzDate = new Date(date.toLocaleString('en-US', { timeZone }));
+  return tzDate.getTime() - utcDate.getTime();
+};
+
 const calendlyDate = '2022-01-26T14:00:00.000000Z';
 console.log('hardcoded:calendlyDate', calendlyDate);
 console.log('hardcoded:fromFunc', utcOffset(calendlyDate));
-console.log('hardcoded:shouldMatch', '2022-01-26T09:00:00.000000-05:00');
-console.log(
-  'hardcoded:shouldMatchLocale',
-  new Date('2022-01-26T09:00:00.000000-05:00').toLocaleString('en-US', {
-    timeZone: 'America/New_York',
-  })
-);
+console.log('hardcoded:shouldMatch', '2022-01-26T09:00:00.000');
