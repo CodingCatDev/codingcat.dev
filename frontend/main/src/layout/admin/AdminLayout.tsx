48358e74ef524a4c9d7e34e53311ee190fd2f5af
Alex Patterson <alex@codingcat.dev>
2022-02-05 19:26:00 -0500

    remove theme provider

--- frontend/main/src/layout/admin/AdminLayout.tsx
+++ frontend/main/src/layout/admin/AdminLayout.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import router from 'next/router';
 import Head from 'next/head';
-import { ThemeProvider } from 'next-themes';
 
 import AdminMenu from '@/layout/admin/AdminMenu';
 import { Site } from '@/models/site.model';
@@ -78,40 +77,34 @@ const AdminLayout = ({
         <FirebaseAuthProvider>
           <FirebaseFirestoreProvider>
             <FirebaseFunctionsProvider>
-              <ThemeProvider
-                attribute="class"
-                storageKey="nightwind-mode"
-                defaultTheme="light"
-              >
-                <div className="lg:mx-auto lg:w-full lg:max-w-8xl">
-                  <Progress isAnimating={isNavigating} />
-                  <div className="grid h-screen grid-cols-admin">
-                    <AdminMenu
+              <div className="lg:mx-auto lg:w-full lg:max-w-8xl">
+                <Progress isAnimating={isNavigating} />
+                <div className="grid h-screen grid-cols-admin">
+                  <AdminMenu
+                    userMenu={userMenu}
+                    setUserMenu={setUserMenu}
+                    navOpen={navOpen}
+                    setNavOpen={setNavOpen}
+                  />
+
+                  <div className="grid items-start h-full grid-cols-1 grid-rows-admin">
+                    <AdminHeader
+                      site={site}
+                      post={post}
                       userMenu={userMenu}
                       setUserMenu={setUserMenu}
                       navOpen={navOpen}
                       setNavOpen={setNavOpen}
                     />
-
-                    <div className="grid items-start h-full grid-cols-1 grid-rows-admin">
-                      <AdminHeader
-                        site={site}
-                        post={post}
-                        userMenu={userMenu}
-                        setUserMenu={setUserMenu}
-                        navOpen={navOpen}
-                        setNavOpen={setNavOpen}
-                      />
-                      <main className="grid h-full text-primary-900 bg-primary-50 dark:bg-basics-900">
-                        {children}
-                        <footer className="flex items-start self-end justify-center p-4">
-                          &copy; CodingCatDev {new Date().getFullYear()}
-                        </footer>
-                      </main>
-                    </div>
+                    <main className="grid h-full text-primary-900 bg-primary-50 dark:bg-basics-900">
+                      {children}
+                      <footer className="flex items-start self-end justify-center p-4">
+                        &copy; CodingCatDev {new Date().getFullYear()}
+                      </footer>
+                    </main>
                   </div>
                 </div>
-              </ThemeProvider>
+              </div>
             </FirebaseFunctionsProvider>
           </FirebaseFirestoreProvider>
         </FirebaseAuthProvider>
