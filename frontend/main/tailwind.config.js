36b6237ba6871fd8d54643e573e3f66bccd94483
Alex Patterson <alex@codingcat.dev>
2022-06-14 20:23:38 -0400

    dark mode based on class

--- frontend/main/tailwind.config.js
+++ frontend/main/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   presets: [require('./ccd-preset.js')],
   content: ['./src/**/*.{js,mdx,jsx,tsx}', './next.config.js'],
   plugins: [require('@tailwindcss/forms')],
