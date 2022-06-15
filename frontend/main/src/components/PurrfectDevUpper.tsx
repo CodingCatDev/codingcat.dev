0fd1412d9e00e8e5cf5066eb8ddbf3d2a66ffa55
Alex Patterson <alex@codingcat.dev>
2022-06-15 12:04:31 -0400

    add listener support

--- frontend/main/src/components/PurrfectDevUpper.tsx
+++ frontend/main/src/components/PurrfectDevUpper.tsx
@@ -23,8 +23,8 @@ export default function PurrfectDevUpper(): JSX.Element {
           <div className="flex flex-wrap gap-4">
             <Link href="/sponsorship">
               <a>
-                <button className="flex items-center btn-secondary-reverse">
-                  Ad Sponsorship
+                <button className="flex items-center gap-2 btn-primary">
+                  Listener Support
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="w-6 h-6"
@@ -39,6 +39,13 @@ export default function PurrfectDevUpper(): JSX.Element {
                       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                     />
                   </svg>
+                </button>
+              </a>
+            </Link>
+            <Link href="/sponsorship">
+              <a>
+                <button className="flex items-center btn-secondary-reverse">
+                  Ad Sponsorship
                 </button>
               </a>
             </Link>
