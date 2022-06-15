7fb18018e0b0a85eb9a8ed09a1c8b5e2ae355920
Alex Patterson <alex@codingcat.dev>
2022-06-15 14:09:24 -0400

    fix support link

--- frontend/main/src/components/PurrfectDevUpper.tsx
+++ frontend/main/src/components/PurrfectDevUpper.tsx
@@ -21,27 +21,25 @@ export default function PurrfectDevUpper(): JSX.Element {
             .
           </p>
           <div className="flex flex-wrap gap-4">
-            <Link href="/sponsorship">
-              <a>
-                <button className="flex items-center gap-2 btn-primary">
-                  Listener Support
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </button>
-              </a>
-            </Link>
+            <a href="https://anchor.fm/purrfect-dev/support">
+              <button className="flex items-center gap-2 btn-primary">
+                Listener Support
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </button>
+            </a>
             <Link href="/sponsorship">
               <a>
                 <button className="flex items-center btn-secondary-reverse">
