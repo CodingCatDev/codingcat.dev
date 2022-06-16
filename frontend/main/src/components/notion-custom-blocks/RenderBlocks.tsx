aea179d09f23761e2533cc5e0cc413319b599da2
Alex Patterson <alex@codingcat.dev>
2022-06-16 16:52:30 -0400

    take image out for now

--- frontend/main/src/components/notion-custom-blocks/RenderBlocks.tsx
+++ frontend/main/src/components/notion-custom-blocks/RenderBlocks.tsx
@@ -7,25 +7,26 @@ import CodeHighlight from '@/components/notion-custom-blocks/CodeHighlight';
 import Image from 'next/image';
 
 const myMapper = {
-  image: withContentValidation(({ className, media }) => {
-    return media ? (
-      // <div className="flex flex-col" style={{ maxWidth: 'calc(100vw - 40px)' }}>
-      //   <div className="relative flex-1 block mb-2 overflow-hidden aspect-video ">
-      <Image
-        loader={({ src }) => src}
-        className={className || ''}
-        src={media.src}
-        alt={media?.alt || media?.name || ''}
-        layout="fill"
-        height="100%"
-        width="100%"
-      />
-    ) : (
-      <></>
-    );
-  }),
-  code: withContentValidation(({ plainText, className }) =>
-    CodeHighlight({ plainText, className })
+  // image: withContentValidation(({ className, media }) => {
+  //   return media ? (
+  //     // <div className="flex flex-col" style={{ maxWidth: 'calc(100vw - 40px)' }}>
+  //     //   <div className="relative flex-1 block mb-2 overflow-hidden aspect-video ">
+  //     <Image
+  //       loader={({ src }) => src}
+  //       className={className || ''}
+  //       src={media.src}
+  //       alt={media?.alt || media?.name || ''}
+  //       layout="fill"
+  //       height="100%"
+  //       width="100%"
+  //     />
+  //   ) : (
+  //     <></>
+  //   );
+  // }),
+  code: withContentValidation(
+    ({ plainText, className }: { plainText: string; className: string }) =>
+      CodeHighlight({ plainText, className })
   ),
 };
 
