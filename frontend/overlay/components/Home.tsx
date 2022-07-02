5aff2510260b5b8e9c49a4fff13c489c44345a33
Alex Patterson <alex@codingcat.dev>
2022-07-02 16:09:38 -0400

    scrolling working

--- frontend/overlay/components/Home.tsx
+++ frontend/overlay/components/Home.tsx
@@ -11,6 +11,7 @@ import AJPrimary from '../components/AJPrimary';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
+import { useEffect, useRef, useState } from 'react';
 
 const httpLink = new HttpLink({
 	uri: 'https://codingcat-twitch.onrender.com/graphql'
@@ -63,36 +64,51 @@ const MESSAGES_SUBSCRIPTION = gql`
 	}
 `;
 
-function LatestMessage(client: any) {
+export default function Home() {
 	const { data, loading } = useSubscription(MESSAGES_SUBSCRIPTION, {
 		client,
 		variables: { username: 'codingcatdev' }
 	});
-	console.log(loading);
-	console.log(data);
-	return (
-		<>
-			{loading && data?.message?.message ? (
-				<>Loading...</>
-			) : (
-				<>
-					<div className="flex">
-						<div>{data?.message?.message}</div>
-					</div>
-				</>
-			)}
-		</>
-	);
-}
+	const bottomRef = useRef<HTMLDivElement>(null);
+	const [chat, setChat] = useState<any>([]);
 
-export default function Home() {
+	useEffect(() => {
+		if (data && data?.message?.id) {
+			if (!chat.includes(data?.message?.id)) {
+				setChat((c: any) => [...c, data.message]);
+			}
+		}
+		bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+		bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, [data]);
+
 	return (
 		<ApolloProvider client={client}>
 			<main className="grid grid-rows-[1fr_8px_144px] h-screen overflow-hidden">
 				<div className="overlay-top h-full w-full" />
 				<div className="w-full bg-purple-900 h-2" />
 				<div className="w-full bg-gradient-to-r to-purple-700 via-purple-500 from-pink-500 flex justify-end">
-					<div className="flex flex-grow">{LatestMessage(client)}</div>
+					<div className="flex flex-grow">
+						<div className="messages flex flex-col overflow-y-scroll max-w-xl p-1 relative">
+							{loading ? (
+								<div className="rounded-xl p-1 bg-purple-500 text-purple-50 px-1">
+									No Messages Yet...
+								</div>
+							) : (
+								<>
+									{chat.map((c: any) => (
+										<div key={c.id} className="flex gap-1">
+											<div className="rounded bg-purple-500 text-purple-50 px-1">
+												{c?.author?.username}
+											</div>
+											<div className="text-purple-50">{c.message}</div>
+										</div>
+									))}
+									<div ref={bottomRef} className="mt-6" />
+								</>
+							)}
+						</div>
+					</div>
 					<div className="w-36 h-36 p-2">
 						<AJPrimary className="block w-32 h-32" />
 					</div>
