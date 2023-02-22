dc2d5b454c776620130bda554cdb7d9f5441450a
Alex <alex@codingcat.dev>
2023-02-22 17:58:00 -0500

    add eslint correct for turbo

--- apps/firebase/functions/.eslintrc.js
+++ apps/firebase/functions/.eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'import'],
