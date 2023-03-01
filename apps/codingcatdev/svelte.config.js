b4a579aba1557692b67bc4826c9992ac373fac49
Alex <alex@codingcat.dev>
2023-03-01 15:40:06 -0500

    need to remove content from prod builds

--- apps/codingcatdev/svelte.config.js
+++ apps/codingcatdev/svelte.config.js
@@ -2,35 +2,21 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex';
 
+// TODO: remove .svx and .md from production builds
+
 /** @type {import('@sveltejs/kit').Config} */
-let config;
-
-if (process.env.NODE_ENV === 'development') {
-	console.log(`Using ${process.env.NODE_ENV} config`);
-	config = {
-		kit: {
-			adapter: adapter()
-		},
-		extensions: ['.svelte', '.svx', '.md'],
-		preprocess: [
-			mdsvex({ extensions: ['.svx', '.md'] }),
-			preprocess({
-				postcss: true
-			})
-		]
-	};
-} else {
-	config = {
-		kit: {
-			adapter: adapter()
-		},
-
-		preprocess: [
-			preprocess({
-				postcss: true
-			})
-		]
-	};
-}
+console.log(`Using ${process.env.NODE_ENV} config`);
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	extensions: ['.svelte', '.svx', '.md'],
+	preprocess: [
+		mdsvex({ extensions: ['.svx', '.md'] }),
+		preprocess({
+			postcss: true
+		})
+	]
+};
 
 export default config;
