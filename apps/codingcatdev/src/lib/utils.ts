497a58bd4231812302a1fc94fac3dd5d3703e41f
Alex <alex@codingcat.dev>
2023-03-02 21:34:23 -0500

    add utils

--- apps/codingcatdev/src/lib/utils.ts
+++ apps/codingcatdev/src/lib/utils.ts
@@ -1,4 +1,20 @@
-/** @param {string} name */
+import { ContentType, type Content } from './types';
+
 export function get_depth(name: string) {
 	return name.split('/').length - 1;
 }
+
+export const contentLists = [
+	...Object.values(ContentType)
+		.filter((t) => ![ContentType.post, ContentType.page].includes(t))
+		.map((t) => `${t}s`),
+	'blog'
+];
+
+export const pluralize = (content: Content) => {
+	return `${content.type === ContentType.post ? 'blog' : content.type + 's'}`;
+};
+
+export const unpluralize = (slug: string) => {
+	return `${slug === 'blog' ? ContentType.post : slug.slice(0, -1)}` as ContentType;
+};
