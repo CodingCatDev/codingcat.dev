8b73e5257f1d1d53b4aa175f5a8d85a19d9ab9cd
Alex <alex@codingcat.dev>
2023-04-16 19:16:25 -0400

    adjust login components

--- apps/codingcatdev/src/routes/login/+page.svelte
+++ apps/codingcatdev/src/routes/login/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
 	import LayoutWrapper from '../(layout-partials)/LayoutWrapper.svelte';
-
-	import { enhance } from '$app/forms';
-	import type { ActionData } from './$types';
-
-	import { ccdSignInWithEmailAndPassword } from '$lib/client/firebase';
-
-	import { toastStore } from '@codingcatdev/blackcatui';
-
 	import GoogleAuth from './GoogleAuth.svelte';
 	import TwitterAuth from './TwitterAuth.svelte';
 	import GitHubAuth from './GitHubAuth.svelte';
@@ -16,68 +8,18 @@
 	// export let form: ActionData;
 
 	import type { PageData } from './$types';
+	import EmailAuth from './EmailAuth.svelte';
 	export let data: PageData;
 	$: action = `?/login&redirectTo=${data.redirectTo ? data.redirectTo : '/dashboard'}`;
 </script>
 
 <LayoutWrapper>
 	<section class="bcu-card flex flex-col gap-4 p-4">
-		<form
-			class="flex justify-center w-full"
-			method="POST"
-			{action}
-			use:enhance={async ({ action, cancel, controller, data, form }) => {
-				// `form` is the `<form>` element
-				// `data` is its `FormData` object
-				// `action` is the URL to which the form is posted
-				// `cancel()` will prevent the submission
-
-				const email = `${data.get('email')}`;
-				const password = `${data.get('password')}`;
-
-				if (!email || !password) {
-					toastStore.trigger({
-						message: 'Please Provide Username and Password.'
-					});
-					cancel();
-				} else {
-					try {
-						await ccdSignInWithEmailAndPassword({ email, password });
-					} catch (err) {
-						if (err instanceof Error) {
-							toastStore.trigger({
-								message: err.message,
-								background: 'variant-filled-error'
-							});
-						}
-					}
-				}
-
-				return async ({ result, update }) => {
-					// `result` is an `ActionResult` object
-					// `update` is a function which triggers the logic that would be triggered if this callback wasn't set
-
-					update();
-				};
-			}}
-		>
-			<div class="flex flex-col gap-4 text-token">
-				<label class="label">
-					<span>Email</span>
-					<input class="input" name="email" type="email" placeholder="codercatdev" />
-				</label>
-				<label class="label">
-					<span>Password</span>
-					<input class="input" name="password" type="password" placeholder="****" />
-				</label>
-				<button class="bcu-button variant-filled-primary" type="submit">Login</button>
-			</div>
-		</form>
-
+		<EmailAuth {action} />
 		<hr />
 
 		<GoogleAuth {action} />
-		<TwitterAuth />
-		<GitHubAuth />
+		<!-- <TwitterAuth /> -->
+		<GitHubAuth {action} />
 	</section>
 </LayoutWrapper>
