3ac1572f0680967043bde08f0e78c50414ac87c9
Alex <alex@codingcat.dev>
2023-04-18 18:33:17 -0400

    block dev users

--- apps/codingcatdev/src/routes/(content-single)/course/[slug]/lesson/[lessonSlug]/+page.server.ts
+++ apps/codingcatdev/src/routes/(content-single)/course/[slug]/lesson/[lessonSlug]/+page.server.ts
@@ -1,3 +1,4 @@
+import { env } from '$env/dynamic/private';
 import { getLessonFromCourseSlug } from '$lib/server/content';
 import { db } from '$lib/server/firebase';
 import { ContentType } from '$lib/types';
@@ -20,24 +21,38 @@ export const load = (async ({ params, parent, url }) => {
 		throw redirect(307, `/login?redirectTo=${url.pathname}`);
 	}
 
-	const snapshot = await db.collection('stripe-products').where('active', '==', true).get();
-	const products = [];
+	let allowedPast = true;
 
-	for (const doc of snapshot.docs) {
-		const priceSnapshot = await doc.ref.collection('prices').where('active', '==', true).get();
+	if (env.VERCEL_ENV !== "production") {
+		const doc = await db.collection('admins').doc(user.uid).get();
+		allowedPast = doc.exists;
+	}
 
-		for (const price of priceSnapshot.docs) {
-			products.push({
-				id: doc.id,
-				...doc.data(),
-				price: price.id
-			})
+	if (!user?.stripeRole && allowedPast) {
+		const snapshot = await db.collection('stripe-products').where('active', '==', true).get();
+		const products = [];
+
+		for (const doc of snapshot.docs) {
+			const priceSnapshot = await doc.ref.collection('prices').where('active', '==', true).get();
+
+			for (const price of priceSnapshot.docs) {
+				products.push({
+					id: doc.id,
+					...doc.data(),
+					price: price.id
+				})
+			}
 		}
+
+		return {
+			products,
+			locked: content?.locked,
+			title: content?.title
+		};
 	}
 
 	return {
 		contentType,
-		content,
-		products
+		content
 	};
 });
--- apps/codingcatdev/src/routes/(content-single)/course/[slug]/lesson/[lessonSlug]/+page.svelte
+++ apps/codingcatdev/src/routes/(content-single)/course/[slug]/lesson/[lessonSlug]/+page.svelte
@@ -6,17 +6,19 @@
 	export let data: PageData;
 </script>
 
-{#if data.content.locked && !data?.user?.stripeRole}
+{#if data.locked && data.products}
 	<section class="flex flex-col justify-center mt-8 items-center p-2">
 		<div class="flex flex-col items-center gap-8">
 			<div class="font-bold flex flex-col gap-2 max-w-4xl">
 				<span class="text-3xl">You have reached a locked lesson.</span>
-				<div class="text-primary-500 text-2xl">{data.content.title}</div>
+				<div class="text-primary-500 text-2xl">{data.title}</div>
 				Select the button below to add a Pro membership to your account.
 			</div>
 			<ProButton products={data.products} uid={data.user?.uid} />
 		</div>
 	</section>
-{:else}
+{:else if data.content}
 	<Lesson {data} />
+{:else}
+	Nice find, we are totally missing something!
 {/if}
