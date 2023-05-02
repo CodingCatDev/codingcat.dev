c0db190efef02decd9917210a86d82aa6bc1d6a4
Matt Gehrls <mattgehrls@gmail.com>
2023-05-02 12:36:04 -0400

    spacing, text changes, dry class variables

--- apps/codingcatdev/src/routes/(protected)/dashboard/DashboardCTA.svelte
+++ apps/codingcatdev/src/routes/(protected)/dashboard/DashboardCTA.svelte
@@ -3,52 +3,59 @@
 	import GitHub from '$lib/components/global/icons/socials/GitHub.svelte';
 	import Twitter from '$lib/components/global/icons/socials/Twitter.svelte';
 	import YouTube from '$lib/components/global/icons/socials/YouTube.svelte';
+
+	const anchorClasses = 'bcu-card bcu-card-hover p-8 min-h-96';
+	const sectionClasses = 'flex flex-col relative gap-4 justify-start items-start h-52 py-14';
+	const logoClasses = 'absolute top-0';
+	const titleClasses = 'tracking-wide';
+	const descriptionClasses = 'text-md font-normal';
 </script>
 
 <!-- TODO hover effects -->
 <div class="w-full py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-	<a
-		class="bcu-card bcu-card-hover p-8 min-h-96"
-		target="_blank"
-		href="https://discord.com/invite/vM2bagU"
-	>
-		<div class="flex flex-col gap-4 justify-center items-start h-52">
-			<Discord />
-			<h5 class="tracking-wide">Join the Discord!</h5>
-			<p class="text-md font-normal">
+	<a class={anchorClasses} target="_blank" href="https://discord.com/invite/vM2bagU">
+		<div class={sectionClasses}>
+			<div class={logoClasses}>
+				<Discord />
+			</div>
+			<h5 class={titleClasses}>Join the Discord!</h5>
+			<p class={descriptionClasses}>
 				We are part of the Teach, Learn, Grow community. A place to learn then give back. Join us!
 			</p>
 		</div>
 	</a>
-	<a class="bcu-card bcu-card-hover p-8" target="_blank" href="https://twitter.com/CodingCatDev">
-		<div class="flex flex-col gap-4 justify-center items-start h-52">
-			<Twitter fill="#BC2261" />
-			<h5 class="tracking-wide">See if Twitter is still running</h5>
-			<p class="text-md font-normal">See the latest and start a conversation.</p>
+	<a class={anchorClasses} target="_blank" href="https://twitter.com/CodingCatDev">
+		<div class={sectionClasses}>
+			<div class={logoClasses}>
+				<Twitter fill="#BC2261" />
+			</div>
+			<h5 class={titleClasses}>Follow Us on Twitter!</h5>
+			<p class={descriptionClasses}>See the latest and start a conversation.</p>
 		</div>
 	</a>
 	<!-- I got the fill value from Discord.svelte, we should talk about color so I can put a style guide together-->
 	<!--make a few more ctas and make them a responsive grid-->
-	<a
-		class="bcu-card bcu-card-hover p-8"
-		target="_blank"
-		href="https://www.youtube.com/c/codingcatdev"
-	>
-		<div class="flex flex-col gap-4 justify-center items-start h-52">
-			<YouTube fill="#BC2261" />
-			<h5 class="tracking-wide">Subscribe to our Youtube</h5>
-			<p class="text-md font-normal">See the latest and start a conversation.</p>
+	<a class={anchorClasses} target="_blank" href="https://www.youtube.com/c/codingcatdev">
+		<div class={sectionClasses}>
+			<div class={logoClasses}>
+				<YouTube fill="#BC2261" />
+			</div>
+			<h5 class={titleClasses}>Subscribe to our Youtube</h5>
+			<p class={descriptionClasses}>See the latest and start a conversation.</p>
 		</div>
 	</a>
+	<!-- last anchor has different classes for responsiveness-->
 	<a
 		class="bcu-card bcu-card-hover p-8 md:col-span-3 md:max-w-full lg:col-span-1 lg:max-w-xs"
 		target="_blank"
 		href="https://github.com/sponsors/CodingCatDev"
 	>
-		<div class="flex flex-col gap-4 justify-center items-start h-52">
-			<GitHub fill="#BC2261" />
-			<h5 class="tracking-wide">Sponsor our GitHub</h5>
-			<p class="text-md font-normal">
+		<div class={sectionClasses}>
+			<div class={logoClasses}>
+				<GitHub fill="#BC2261" />
+			</div>
+			<h5 class={titleClasses}>Sponsor our GitHub</h5>
+			<p class={descriptionClasses}>
 				Star the repo, look at the issues, and sponsor us to help us keep the lights on.
 			</p>
 		</div>
