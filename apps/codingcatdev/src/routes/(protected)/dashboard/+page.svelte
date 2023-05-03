99d9ee6458fb9b6790b78dbd83eb8e4beb32135d
Alex Patterson <alex@codingcat.dev>
2023-05-02 21:53:36 -0400

    Update Dashboard (#443)

--- apps/codingcatdev/src/routes/(protected)/dashboard/+page.svelte
+++ apps/codingcatdev/src/routes/(protected)/dashboard/+page.svelte
@@ -1,24 +1,37 @@
 <script lang="ts">
 	import ProButton from '../ProButton.svelte';
 	import type { PageData } from './$types';
+	import DashboardWelcome from './DashboardWelcome.svelte';
+	import DashboardCTA from './DashboardCTA.svelte';
+
 	export let data: PageData;
 </script>
 
-<div class="flex flex-col gap-4 p-2">
-	<div class="p-4 sm:p-10">
+<div class="flex justify-center p-4">
+	<div class="xl:max-w-7xl mt-10 flex-1">
 		<section class="flex flex-col gap-2 md:gap-8">
 			<div class="flex justify-between">
 				<h1>Dashboard</h1>
-				<a class="bcu-button variant-filled" href="/account">Account & Settings</a>
+				<a class="bcu-button variant-filled" href="/account">Account</a>
 			</div>
-			<div class="flex flex-col md:flex-row gap-2">
-				<div class="flex flex-col w-full gap-2">
-					<div class="bcu-card variant-filled">Welcome Message</div>
-					<div class="bcu-card variant-filled">Join Discord for LBT</div>
-				</div>
+			<div class="flex flex-col items-center md:items-stretch md:flex-row gap-4">
+				<DashboardWelcome {data} />
 				{#if !data?.user?.stripeRole}
 					<ProButton products={data.products} uid={data.user?.uid} />
 				{/if}
+			</div>
+			<DashboardCTA />
+			<div>
+				<h3>✨ New and Featured</h3>
+				<div class="p-4">
+					<p>Coming soon.</p>
+				</div>
+			</div>
+			<div>
+				<h3>📅 Coming Soon</h3>
+				<div class="p-4">
+					<p>Coming soon.</p>
+				</div>
 			</div>
 		</section>
 	</div>
