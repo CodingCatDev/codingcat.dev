528b681bb9ef4da3fba16b2f217be4fc8663fa1f
Alex <alex@codingcat.dev>
2023-05-11 16:37:16 -0400

    fix author in authors

--- apps/codingcatdev/src/routes/(content-list)/authors/+page.server.ts
+++ apps/codingcatdev/src/routes/(content-list)/authors/+page.server.ts
@@ -1,7 +1,7 @@
 import { getContentTypeDirectory, listContent } from '$lib/server/content';
 import { ContentType, type Author } from '$lib/types';
 
-const contentType = ContentType.podcast;
+const contentType = ContentType.author;
 
 export const load = async () => {
 	return {
