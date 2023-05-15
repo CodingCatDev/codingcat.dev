8a86c65834e9a42152a8b120f003b744c19f5d8d
Matt Gehrls <mattgehrls@gmail.com>
2023-05-15 15:37:27 -0400

    fixed wrong href

--- apps/codingcatdev/src/routes/(protected)/dashboard/DashboardCTA.svelte
+++ apps/codingcatdev/src/routes/(protected)/dashboard/DashboardCTA.svelte
@@ -47,7 +47,7 @@
 	<a
 		class="bcu-card bcu-card-hover p-8 md:col-span-3 md:max-w-full lg:col-span-1 lg:max-w-xs"
 		target="_blank"
-		href="https://github.com/codingcatdev/"
+		href="https://github.com/sponsors/CodingCatDev"
 	>
 		<div class={sectionClasses}>
 			<div class={logoClasses}>
