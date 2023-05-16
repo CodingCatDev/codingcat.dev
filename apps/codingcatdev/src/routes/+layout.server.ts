4e6f820cd1540299d5a8c8b70cdd199833f5a16f
Alex Patterson <alex@codingcat.dev>
2023-05-16 10:26:50 -0400

    try without prerender (#446)

--- apps/codingcatdev/src/routes/+layout.server.ts
+++ apps/codingcatdev/src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import { ccdValidateSessionCookie, validateStripeRole } from '$lib/server/firebase';
-export const prerender = true;
+export const prerender = false;
 export const load = (async ({ cookies }) => {
 	try {
 
