cb5a53519be37e2b59ca6186c8f1cc19e149e760
Alex Patterson <alex@codingcat.dev>
2023-05-19 10:51:05 -0400

    Release RSS Fix (#456)
    * fix start date (#453)
    
    * add author and html to rss (#455)

--- apps/codingcatdev/src/routes/(rss)/feed-podcasts.json/+server.ts
+++ apps/codingcatdev/src/routes/(rss)/feed-podcasts.json/+server.ts
@@ -21,10 +21,10 @@ export const GET = async () => {
 	return new Response(
 		buildFeed({
 			contentType, contents: contentItems, authorItems
-		}).rss2(),
+		}).json1(),
 		{
 			headers: {
-				'content-type': 'application/rss+xml', 'cache-control': 'max-age=0, s-maxage=3600',
+				'content-type': 'application/json', 'cache-control': 'max-age=0, s-maxage=3600',
 			},
 		}
 	)
