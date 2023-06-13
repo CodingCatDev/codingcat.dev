ae71210b00fd2691b65fe4ab0c2257b639b2f50e
Alex <alex@codingcat.dev>
2023-06-13 15:54:33 -0400

    fix auth

--- apps/codingcatdev/src/routes/(protected)/account/AccountCard.svelte
+++ apps/codingcatdev/src/routes/(protected)/account/AccountCard.svelte
@@ -4,25 +4,27 @@
 
 	import { openStripePortal } from '$lib/client/firebase';
 	import LogoutButton from '../../login/LogoutButton.svelte';
+	import { Avatar } from '@codingcatdev/blackcatui';
 </script>
 
-<div class="bcu-card flex flex-col md:flex-row justify-between gap-2 md:gap-8 w-full">
-	<div class="flex flex-col md:flex-row md:justify-between w-full p-8 gap-4 md:gap-8">
+<div class="flex flex-col justify-between w-full gap-2 bcu-card md:flex-row md:gap-8">
+	<div class="flex flex-col w-full gap-4 p-8 md:flex-row md:justify-between md:gap-8">
 		<div class="md:w-1/3">
 			<h2 class="mb-8">Account</h2>
 			<p>Your account details</p>
 		</div>
-		<div class="bcu-card variant-filled p-8 flex flex-col gap-4 md:w-2/3">
+		<div class="flex flex-col gap-4 p-8 bcu-card variant-filled md:w-2/3">
 			<div class="flex items-center gap-4">
 				<p class="text-lg">Your Email:</p>
 				<p>{data?.user?.email}</p>
 			</div>
 			<div class="flex items-center gap-4">
 				<p class="text-lg">Your Avatar:</p>
-				<img
-					class="w-20"
+				<Avatar
+					class="w-20 h-20 bcu-avatar-xs"
 					src={data.user ? data.user.picture : '/images/avatar.png'}
-					alt="User's avatar"
+					alt="User Photo"
+					referrerpolicy="no-referrer"
 				/>
 			</div>
 			<LogoutButton />
--- apps/codingcatdev/src/routes/(protected)/dashboard/DashboardWelcome.svelte
+++ apps/codingcatdev/src/routes/(protected)/dashboard/DashboardWelcome.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+	import { Avatar } from '@codingcatdev/blackcatui';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
 
 <div
-	class="bcu-card variant-filled min-h-full flex flex-col w-full justify-center items-start gap-4 p-8"
+	class="flex flex-col items-start justify-center w-full min-h-full gap-4 p-8 bcu-card variant-filled"
 >
 	<div class="flex items-center gap-4">
-		<img src={data.user ? data.user.picture : '/images/avatar.png'} alt="User's avatar" />
+		<Avatar
+			class="w-20 h-20 bcu-avatar-xs"
+			src={data.user ? data.user.picture : '/images/avatar.png'}
+			alt="User Photo"
+			referrerpolicy="no-referrer"
+		/>
 		<h2 class="text-md">Welcome, {data.user?.email}!</h2>
 	</div>
 	<p class="p-2">Now that you're here, get involved!</p>
