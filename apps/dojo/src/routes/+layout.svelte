3ba17b13b62278ba3aef989686e94e7e49b8626d
Alex FusionAuth <alex.patterson@fusionauth.io>
2023-10-13 05:07:21 -0400

    update defaults

--- apps/dojo/src/routes/+layout.svelte
+++ apps/dojo/src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Sneakers Dojo</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
