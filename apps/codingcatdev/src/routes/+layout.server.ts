aa70e9bc5f8ef0c84cb45d272d49a3370663697d
Alex Patterson <alex.patterson@fusionauth.io>
2023-10-26 11:28:58 -0400

    add preview

--- apps/codingcatdev/src/routes/+layout.server.ts
+++ apps/codingcatdev/src/routes/+layout.server.ts
@@ -2,6 +2,8 @@ import { listContent, getContentTypeDirectory } from '$lib/server/content';
 import { ccdValidateSessionCookie, validateStripeRole } from '$lib/server/firebase';
 import { type Content, ContentType } from '$lib/types';
 import type { Cookies } from '@sveltejs/kit';
+import { PREVIEW } from '$env/static/private';
+
 //export const prerender = false;
 export const load = async ({ cookies }: { cookies: Cookies }) => {
 	try {
@@ -29,12 +31,15 @@ export const load = async ({ cookies }: { cookies: Cookies }) => {
 				...user,
 				stripeRole
 			},
-			podcasts
+			podcasts,
+			previewMode: PREVIEW
 		};
 	} catch (error) {
 		cookies.set('session', '', { expires: new Date(0) });
 
 		console.error(error);
-		return {};
+		return {
+			previewMode: PREVIEW
+		};
 	}
 };
