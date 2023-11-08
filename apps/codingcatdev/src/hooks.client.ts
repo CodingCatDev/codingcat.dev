a1d6e5134cba08394c825e630e9c1d0647e07dee
Alex Patterson <alex.patterson@fusionauth.io>
2023-11-08 10:43:31 -0500

    add sentry environments

--- apps/codingcatdev/src/hooks.client.ts
+++ apps/codingcatdev/src/hooks.client.ts
@@ -1,20 +1,22 @@
-import { handleErrorWithSentry, Replay } from "@sentry/sveltekit";
+import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
+import { env } from '$env/dynamic/private';
 
 Sentry.init({
-  dsn: 'https://518fe25472568a2e47252e6f29583c6b@o1029244.ingest.sentry.io/4506190917206016',
-  tracesSampleRate: 1.0,
+	dsn: 'https://518fe25472568a2e47252e6f29583c6b@o1029244.ingest.sentry.io/4506190917206016',
+	tracesSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [new Replay()],
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [new Replay()],
+	environment: env.VERCEL_ENV || 'local'
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- apps/codingcatdev/src/hooks.server.ts
+++ apps/codingcatdev/src/hooks.server.ts
@@ -1,22 +1,24 @@
-import {sequence} from '@sveltejs/kit/hooks';
+import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
 import { redirect, type Handle } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 
 Sentry.init({
-    dsn: "https://518fe25472568a2e47252e6f29583c6b@o1029244.ingest.sentry.io/4506190917206016",
-    tracesSampleRate: 1
-})
+	dsn: 'https://518fe25472568a2e47252e6f29583c6b@o1029244.ingest.sentry.io/4506190917206016',
+	tracesSampleRate: 1,
+	environment: env.VERCEL_ENV || 'local'
+});
 
 export const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
-    if (event.url.pathname.startsWith('/tutorials')) {
-        throw redirect(301, '/posts');
-    }
+	if (event.url.pathname.startsWith('/tutorials')) {
+		throw redirect(301, '/posts');
+	}
 
-    if (event.url.pathname.startsWith('/tutorial')) {
-        throw redirect(301, `/post/${event.url.pathname.split('/').at(-1)}`);
-    }
+	if (event.url.pathname.startsWith('/tutorial')) {
+		throw redirect(301, `/post/${event.url.pathname.split('/').at(-1)}`);
+	}
 
-    const response = await resolve(event);
-    return response;
+	const response = await resolve(event);
+	return response;
 }) satisfies Handle);
 export const handleError = Sentry.handleErrorWithSentry();
