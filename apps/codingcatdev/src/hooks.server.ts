39517d7993d91cff42d269f949423dd40c544578
Alex Patterson <alex@codingcat.dev>
2023-11-08 12:20:22 -0500

    Feature/sentry (#506)
    * add sentry
    
    * add sentry environments
    
    ---------
    
    Co-authored-by: Alex Patterson <alex.patterson@fusionauth.io>

--- apps/codingcatdev/src/hooks.server.ts
+++ apps/codingcatdev/src/hooks.server.ts
@@ -1,14 +1,24 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { redirect, type Handle } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 
-export const handle = (async ({ event, resolve }) => {
-    if (event.url.pathname.startsWith('/tutorials')) {
-        throw redirect(301, '/posts');
-    }
+Sentry.init({
+	dsn: 'https://518fe25472568a2e47252e6f29583c6b@o1029244.ingest.sentry.io/4506190917206016',
+	tracesSampleRate: 1,
+	environment: env.VERCEL_ENV || 'local'
+});
 
-    if (event.url.pathname.startsWith('/tutorial')) {
-        throw redirect(301, `/post/${event.url.pathname.split('/').at(-1)}`);
-    }
+export const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/tutorials')) {
+		throw redirect(301, '/posts');
+	}
 
-    const response = await resolve(event);
-    return response;
-}) satisfies Handle;
+	if (event.url.pathname.startsWith('/tutorial')) {
+		throw redirect(301, `/post/${event.url.pathname.split('/').at(-1)}`);
+	}
+
+	const response = await resolve(event);
+	return response;
+}) satisfies Handle);
+export const handleError = Sentry.handleErrorWithSentry();
--- apps/codingcatdev/vite.config.js
+++ apps/codingcatdev/vite.config.js
@@ -1,12 +1,20 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
+    plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "codingcatdev",
+            project: "codingcatdev-main"
+        }
+    }), sveltekit()],
+
+    test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	ssr: {
+
+    ssr: {
 		noExternal: ['gsap', '@gsap/shockingly', '@cloudinary/html']
 	}
 };
