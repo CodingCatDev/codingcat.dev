316d5cc42f77be85789539916dcab32583daad92
Alex Patterson <alex.patterson@fusionauth.io>
2023-11-15 20:10:48 -0500

    add shorts

--- apps/codingcatdev/src/lib/components/content/Video.svelte
+++ apps/codingcatdev/src/lib/components/content/Video.svelte
@@ -3,20 +3,35 @@
 	export let title: string;
 
 	const youtubeParser = (url: string) => {
-		var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-		var match = url.match(regExp);
+		const regExp =
+			/.*(?:youtu.be\/|(?:youtube.com\/live\/)|(?:youtube.com\/shorts\/)|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+		let match = url.match(regExp);
 		return match && match[1].length == 11 ? match[1] : false;
 	};
 	$: finalSrc = `https://www.youtube.com/embed/${youtubeParser(src)}`;
+	$: isShort = src.includes('short');
 </script>
 
-<div class="aspect-video">
-	<iframe
-		src={finalSrc}
-		{title}
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-		class={`w-full h-full ${$$props.class}`}
-	/>
-</div>
+{#if isShort}
+	<div class="aspect-[9/16]">
+		<iframe
+			src={finalSrc}
+			{title}
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+			class={`w-full h-full ${$$props.class}`}
+		></iframe>
+	</div>
+{:else}
+	<div class="aspect-video">
+		<iframe
+			src={finalSrc}
+			{title}
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			class={`w-full h-full ${$$props.class}`}
+		/>
+	</div>
+{/if}
