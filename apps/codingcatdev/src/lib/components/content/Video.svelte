f561d02ecc76b84a889d886e2e82f2b56e2cb094
Alex Patterson <alex@codingcat.dev>
2023-11-15 22:55:15 -0500

    Feature/cwcc angular 17 impressions (#526)
    * add shorts
    
    * add angular post
    
    * add latest
    
    ---------
    
    Co-authored-by: Alex Patterson <alex.patterson@fusionauth.io>

--- apps/codingcatdev/src/lib/components/content/Video.svelte
+++ apps/codingcatdev/src/lib/components/content/Video.svelte
@@ -3,20 +3,39 @@
 	export let title: string;
 
 	const youtubeParser = (url: string) => {
-		var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-		var match = url.match(regExp);
+		const regExp =
+			/.*(?:youtu.be\/|(?:youtube.com\/live\/)|(?:youtube.com\/shorts\/)|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+		let match = url.match(regExp);
 		return match && match[1].length == 11 ? match[1] : false;
 	};
 	$: finalSrc = `https://www.youtube.com/embed/${youtubeParser(src)}`;
+	$: isShort = src.includes('short');
 </script>
 
-<div class="aspect-video">
-	<iframe
-		src={finalSrc}
-		{title}
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-		class={`w-full h-full ${$$props.class}`}
-	/>
-</div>
+{#if isShort}
+	<div class="flex justify-center">
+		<div class="aspect-[9/16] flex-1 max-w-md">
+			<iframe
+				src={finalSrc}
+				{title}
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowfullscreen
+				class={`w-full h-full rounded-md ${$$props.class}`}
+				loading="lazy"
+			></iframe>
+		</div>
+	</div>
+{:else}
+	<div class="aspect-video">
+		<iframe
+			src={finalSrc}
+			{title}
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			class={`w-full h-full rounded-md ${$$props.class}`}
+			loading="lazy"
+		/>
+	</div>
+{/if}
