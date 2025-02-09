ab12ee2c129edab7cdfb93a11e47d2bebb1678eb
Alex Patterson <alex@codingcat.dev>
2025-02-09 16:08:03 -0500

    cloudinary issue but no stega

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -10,14 +10,14 @@ export const client = createClient({
   stega: {
     studioUrl,
     // logger: console,
-    filter: (props) => {
-      const type = props.sourcePath.at(0) as string;
-      const block = props.sourcePath.at(-1) as string;
-      // If it is content but not of specified types
-      if (!["content"].includes(type) || ["public_id", "secure_url"].includes(block)) {
-        return false;
-      }
-      return props.filterDefault(props);
-    },
+    // filter: (props) => {
+    //   const type = props.sourcePath.at(0) as string;
+    //   const block = props.sourcePath.at(-1) as string;
+    //   // If it is content but not of specified types
+    //   if (!["content"].includes(type) || ["public_id", "secure_url"].includes(block)) {
+    //     return false;
+    //   }
+    //   return props.filterDefault(props);
+    // },
   },
 });
